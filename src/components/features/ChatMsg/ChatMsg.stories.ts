a8ebcab44a697becf30e0c337a6d7d2eba7275ec
JW-Ahn0 <jun.woo.an0@gmail.com>
2024-10-28 14:23:09 +0900

    design: 채팅 메시지 컴포넌트 추가

---
+++ src/components/features/ChatMsg/ChatMsg.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ChatMsg from "./ChatMsg";
+
+const meta = {
+  title: "ChatMsg",
+  component: ChatMsg,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ChatMsg>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultOther: Story = {
+  args: {
+    msg: "안녕하세요 반갑습니다.",
+    isMe: false,
+    creadtedAt: new Date(),
+    isRead: false,
+    isLastMsg: true,
+  },
+};
+
+export const DefaultMe: Story = {
+  args: {
+    msg: "네 안녕하세요 무슨 일이시죠?",
+    isMe: true,
+    creadtedAt: new Date(),
+    isRead: false,
+    isLastMsg: true,
+  },
+};
---
+++ src/components/features/ChatMsg/ChatMsg.styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+const ChatMsgWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: end;
+  gap: 4px;
+
+  .msg {
+    display: inline-block;
+    border-radius: 16px;
+    padding: 8px 12px;
+  }
+
+  .my {
+    background-color: var(--color-chat-orange);
+    color: var(--color-white);
+  }
+  .other {
+    order: -1;
+    background-color: var(--color-gray-light);
+  }
+
+  .time {
+    font-size: 8px;
+    color: var(--color-chat-gray);
+  }
+  .msg--info--con {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .not--read::before {
+    content: "1";
+    font-size: 12px;
+    color: var(--color-chat-yellow);
+  }
+
+  .text--end {
+    text-align: end;
+  }
+  .text--start {
+    text-align: start;
+  }
+`;
+
+export default ChatMsgWrapper;
