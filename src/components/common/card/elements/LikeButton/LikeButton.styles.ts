6fd095cb345ffc7f0fd253ad6be5dd035feefc07
ayoung <a01259@naver.com>
2024-10-29 11:47:35 +0900

    design: 관심글 버튼 컴포넌트 구현

---
+++ src/components/common/card/elements/LikeButton/LikeButton.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledLikeButton = styled.div`
+  width: 35px;
+  height: 35px;
+  border: 1px solid var(--bg-gray-light);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const IconWrapper = styled.span`
+  color: var(--primary-color-org);
+  font-size: var(--font-size-body-medium);
+  line-height: 1;
+`;
