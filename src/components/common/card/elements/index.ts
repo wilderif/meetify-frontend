b17289c500c92e4373cf5485f831e6f02dc38198
ayoung <a01259@naver.com>
2024-10-29 17:10:02 +0900

    design: 카드 컴포넌트, 카드 컨테이너 컴포넌트 구현 - 임시 더미데이터 사용

---
+++ src/components/common/card/elements/index.ts
@@ -0,0 +1,5 @@
+export { default as LikeButton } from "./LikeButton/LikeButton";
+export { default as NewTag } from "./NewTag/NewTag";
+export { default as PositionTagContainer } from "./PositionTagContainer/PositionTagContainer";
+export { default as PostTag } from "./PostTag/PostTag";
+export { default as TechIconContainer } from "./TechIconContainer/TechIconContainer";
