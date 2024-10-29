b7f377469695a5ca72e4da0ea73bd1aaa5021043
ayoung <a01259@naver.com>
2024-10-29 11:48:01 +0900

    design: 모집포지션 태그 컴포넌트 구현

---
+++ src/components/common/card/elements/PositionTag/PositionTag.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledPositionTag = styled.span`
+  padding: 4px 16px;
+  border-radius: 30px;
+  font-size: var(--font-size-body-small);
+  font-weight: var(--font-weight-bold);
+  background-color: var(--bg-purple-light);
+  color: var(--color-purple);
+`;
