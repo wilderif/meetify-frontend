7a980b5c9cdb7f3eaeda47dfc8d9cb6ba8dfd146
wilderif <woojuro3@gmail.com>
2024-10-30 12:49:34 +0900

    design: MyInfoEditPage 컴포넌트 레이아웃
    - 공통 컴포넌트 활용하여 작업
    - style 위한 MyInfoEditPage.styled.ts 파일 작성

---
+++ src/pages/MyInfoPage/MyInfoEditPage/MyInfoEditPage.styled.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MyInfoEditPageWrapper = styled.div`
+  margin: 4rem auto;
+  width: 600px;
+  background-color: yellow;
+`;
+
+export const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
--- src/pages/MyInfoPage/MyInfoEditPage/MyInfoEditPage.tsx
+++ src/pages/MyInfoPage/MyInfoEditPage/MyInfoEditPage.tsx
@@ -1,5 +1,90 @@
+import {
+  ButtonContainer,
+  MyInfoEditPageWrapper,
+  ProfileContainer,
+} from "./MyInfoEditPage.styled";
+import ProfileImage from "../../../components/common/ProfileImage/ProfileImage";
+import CustomSelect from "../../../components/common/CustomSelect/CustomSelect";
+import Input from "../../../components/common/input/Input";
+import TextArea from "../../../components/common/TextArea/TextArea";
+import Button from "../../../components/common/button/Button";
+
+import {
+  기술스택 as interests,
+  소속 as affiliation,
+  모집포지션 as position,
+} from "../../../constants/options";
+
+import DummyProfileImage from "../../../assets/profile-image/dummy-profile-image.png";
+
+/**
+ * CustomSelect 컴포넌트의 width 값이 600px로 고정되어 있어,
+ * 우선 MyInfoEditPageWrapper의 width 값을 600px로 설정하였습니다.
+ *
+ * placeholder
+ * - 사용자 정보 유무에 따라 default 값과 db의 값 선택하여 설정
+ *
+ * Input, TextArea onChange 함수 작성해야 글이 적어짐
+ * -> 공통적으로 사용할 함수 작성할 것
+ *
+ */
 const MyInfoEditPage = () => {
-  return <div>마이페이지 수정</div>;
+  return (
+    <MyInfoEditPageWrapper>
+      <ProfileContainer>
+        <ProfileImage
+          src={DummyProfileImage}
+          alt="user profile image"
+          usageType="userInformation"
+        />
+        User Name 님 환영해요.
+      </ProfileContainer>
+      <Input label="닉네임" placeholder="" value="" onChange={() => {}} />
+
+      <CustomSelect
+        label="직무"
+        options={position}
+        placeholder=""
+        onChange={() => {}}
+        value={undefined}
+        isMulti={false}
+        variant="default"
+      />
+      <CustomSelect
+        label="소속"
+        options={affiliation}
+        placeholder=""
+        onChange={() => {}}
+        value={undefined}
+        isMulti={false}
+        variant="default"
+      />
+      <TextArea label="자기소개" placeholder="" value="" onChange={() => {}} />
+      <CustomSelect
+        label="관심분야"
+        options={interests}
+        placeholder=""
+        onChange={() => {}}
+        value={undefined}
+        isMulti={true}
+        variant="default"
+      />
+      <ButtonContainer>
+        <Button
+          label="회원 탈퇴"
+          buttonType="outline"
+          buttonSize="medium"
+          onClick={() => {}}
+        />
+        <Button
+          label="프로필 저장"
+          buttonType="fill"
+          buttonSize="medium"
+          onClick={() => {}}
+        />
+      </ButtonContainer>
+    </MyInfoEditPageWrapper>
+  );
 };
 
 export default MyInfoEditPage;
