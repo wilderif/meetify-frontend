6f62aa1021f6d068886fbfab64e9c0ddb07a5ef8
ayoung <a01259@naver.com>
2024-10-31 22:35:19 +0900

    fix: 메인화면 max-width 값 변경 및 카드컴포넌트 스타일 조정

--- src/components/common/card/CardContainer.style.ts
+++ src/components/common/card/CardContainer.style.ts
@@ -4,10 +4,10 @@ export const StyledCardContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(2, 1fr);
-  gap: 3rem;
+  gap: 1rem;
   justify-content: center;
   align-items: center;
   width: 100%;
-  max-width: 1600px;
+  max-width: var(--max-width);
   margin: 0 auto;
 `;
