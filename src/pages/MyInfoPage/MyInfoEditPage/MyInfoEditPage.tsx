d46bb6818cce2178bd1bea03a707fb24e702046b
wilderif <woojuro3@gmail.com>
2024-11-04 12:41:27 +0900

    feat: MyInfoEditPage 페이지 기능 추가
    - 페이지 접근 시 useEffect 사용하여 db 정보 state에 저장
    - 저장 버튼, 회원 탈퇴 버튼 기능 추가
    - DB에 데이터가 있다면 데이터를 input 화면에 보여주고 없다면 placeholder 보여주도록 구성

--- src/pages/MyInfoPage/MyInfoEditPage/MyInfoEditPage.tsx
+++ src/pages/MyInfoPage/MyInfoEditPage/MyInfoEditPage.tsx
@@ -1,24 +1,27 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { SelectOption } from "../../../types/types";
 import {
   ButtonContainer,
   MyInfoEditPageWrapper,
   ProfileContainer,
 } from "./MyInfoEditPage.styled";
+import useAuthStore from "../../../store/useAuthStore";
 import ProfileImage from "../../../components/common/ProfileImage/ProfileImage";
 import CustomSelect from "../../../components/common/CustomSelect/CustomSelect";
 import Input from "../../../components/common/input/Input";
 import TextArea from "../../../components/common/TextArea/TextArea";
 import Button from "../../../components/common/button/Button";
-
+import { SelectOption } from "../../../types/types";
 import {
   InterestsOptions,
   AffiliationOptions,
   PositionOptions,
 } from "../../../constants/options";
+import { fetchUserProfile } from "../../../services/userProfile/fetchUserProfile";
+import { saveUserProfile } from "../../../services/userProfile/saveUserProfile";
 
 import DummyProfileImage from "../../../assets/profile-image/dummy-profile-image.png";
+import useAuthApi from "../../../hooks/useAuthApi";
 
 /**
  * CustomSelect 컴포넌트의 width 값이 600px로 고정되어 있어,
@@ -30,11 +33,9 @@ import DummyProfileImage from "../../../assets/profile-image/dummy-profile-image
  * - 전역 상태와 백앤드 로직 작업 후 수정
  *
  * 프로필 이미지 수정 기능, 수정 아이콘 추가
- *
- * Dropdown 컴포넌트의 프로필 눌렀을 때 navigate edit page로 변경
- *
  */
 const MyInfoEditPage = () => {
+  const loginEmail = useAuthStore((state) => state.email);
   const [userInformation, setUserInformation] = useState({
     inputNickname: "",
     selectPosition: {} as SelectOption,
@@ -42,36 +43,82 @@ const MyInfoEditPage = () => {
     inputIntroduction: "",
     selectInterests: [] as SelectOption[],
   });
-
   const navigate = useNavigate();
 
-  // console.log(userInformation);
+  const { deleteUser } = useAuthApi();
 
   /**
    * TODO:
    * useEffect 내부에서 초기에 사용자 정보를 불러오는 함수 호출
    * db에 사용자 정보가 있다면 state에 저장
    */
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const fetchUserProfileData = async () => {
+      try {
+        if (!loginEmail) {
+          throw new Error("로그인한 사용자 정보가 없습니다.");
+        }
+
+        const userProfileData = await fetchUserProfile(loginEmail);
+
+        console.log("userProfileData", userProfileData);
+
+        setUserInformation({
+          inputNickname: userProfileData.nickname,
+          selectPosition: PositionOptions.find(
+            (option) =>
+              (option.value as string).toUpperCase() ===
+              userProfileData.position
+          ) as SelectOption,
+          selectAffiliation: AffiliationOptions.find(
+            (option) =>
+              (option.value as string).toUpperCase() ===
+              userProfileData.affiliation
+          ) as SelectOption,
+          inputIntroduction: userProfileData.bio,
+          selectInterests: InterestsOptions.filter((option) =>
+            userProfileData.interests.includes(
+              (option.value as string).toUpperCase()
+            )
+          ),
+        });
+      } catch (error) {
+        console.error("프로필 불러오기 중 오류 발생:", error);
+      }
+    };
+
+    fetchUserProfileData();
+    console.log("userInformation", userInformation);
+  }, []);
 
   /**
    * TODO:
-   * 사용자 정보 삭제 훅이나 api 사용
-   * 로그인 전역 상태 초기화
-   * 메인 페이지로 이동
+   * 혜수님 로그인 전역 상태 초기화 작업 추가되면 테스트 다시
    */
-  const handleDeleteUser = () => {
+  const handleDeleteUser = async () => {
+    deleteUser(loginEmail);
     navigate("/");
   };
 
   /**
    * TODO:
-   * state에 저장된 사용자 정보 백앤드로 전송 후 db에 저장
    * 프로필 확인 기능 추가된다면 프로필 페이지로 이동
    * 프로필 확인 기능 없다면 메인 페이지로 이동
    */
-  const handleSaveProfile = () => {
-    navigate("/");
+  const handleSaveProfile = async () => {
+    try {
+      await saveUserProfile(
+        loginEmail,
+        userInformation.inputNickname,
+        userInformation.selectPosition,
+        userInformation.selectAffiliation,
+        userInformation.inputIntroduction,
+        userInformation.selectInterests
+      );
+      navigate("/");
+    } catch (error) {
+      console.error("프로필 저장 중 오류 발생:", error);
+    }
   };
 
   // 닉네임, 자기소개 input 값 변경 시 state에 저장
@@ -97,10 +144,7 @@ const MyInfoEditPage = () => {
         {
           return {
             ...prevState,
-            [selectType]:
-              selectType === "selectInterests"
-                ? (selectedOption as SelectOption[]).map((option) => option)
-                : (selectedOption as SelectOption),
+            [selectType]: selectedOption as SelectOption,
           };
         }
       });
@@ -119,7 +163,11 @@ const MyInfoEditPage = () => {
       </ProfileContainer>
       <Input
         label="닉네임"
-        placeholder=""
+        placeholder={
+          userInformation.inputNickname
+            ? userInformation.inputNickname
+            : "닉네임을 입력해주세요."
+        }
         value={userInformation.inputNickname}
         onChange={handleInputChange("inputNickname")}
       />
@@ -127,33 +175,55 @@ const MyInfoEditPage = () => {
       <CustomSelect
         label="직무"
         options={PositionOptions}
-        placeholder=""
+        placeholder={
+          userInformation.selectPosition
+            ? userInformation.selectPosition.label
+            : "직무를 선택해주세요."
+        }
         onChange={handleSelectChange("selectPosition")}
-        value={userInformation.selectPosition}
+        value={userInformation.selectPosition || null}
         isMulti={false}
         variant="default"
       />
       <CustomSelect
         label="소속"
         options={AffiliationOptions}
-        placeholder=""
+        placeholder={
+          userInformation.selectAffiliation
+            ? userInformation.selectAffiliation.label
+            : "소속을 선택해주세요."
+        }
         onChange={handleSelectChange("selectAffiliation")}
-        value={userInformation.selectAffiliation}
+        value={userInformation.selectAffiliation || null}
         isMulti={false}
         variant="default"
       />
       <TextArea
         label="자기소개"
-        placeholder=""
+        placeholder={
+          (console.log(
+            "userInformation.inputIntroduction",
+            userInformation.inputIntroduction
+          ),
+          userInformation.inputIntroduction
+            ? userInformation.inputIntroduction
+            : "자기소개를 입력해주세요.")
+        }
         value={userInformation.inputIntroduction}
         onChange={handleInputChange("inputIntroduction")}
       />
       <CustomSelect
         label="관심분야"
         options={InterestsOptions}
-        placeholder=""
+        placeholder={
+          userInformation.selectInterests.length > 0
+            ? userInformation.selectInterests
+                .map((option) => option.label)
+                .join(", ")
+            : "관심분야를 선택해주세요."
+        }
         onChange={handleSelectChange("selectInterests")}
-        value={userInformation.selectInterests}
+        value={userInformation.selectInterests || null}
         isMulti={true}
         variant="default"
       />
--- src/services/userProfile/saveUserProfile.ts
+++ src/services/userProfile/saveUserProfile.ts
@@ -11,15 +11,24 @@ export const saveUserProfile = async (
   interests: SelectOption[]
 ) => {
   try {
+    if (!nickname) {
+      throw new Error("닉네임을 입력해주세요.");
+    }
     const response = await axios.patch(
       "http://localhost:3000/api/user-profile",
       {
         email,
         nickname,
-        position: position.label,
-        affiliation: affiliation.label,
-        introduction,
-        interests: interests.map((interest) => interest.label),
+        position: position
+          ? (position.value as string).toUpperCase()
+          : undefined,
+        affiliation: affiliation
+          ? (affiliation.value as string).toUpperCase()
+          : undefined,
+        bio: introduction,
+        interests: interests.map((interest) =>
+          (interest.value as string).toUpperCase()
+        ),
       }
     );
     return response.data;
