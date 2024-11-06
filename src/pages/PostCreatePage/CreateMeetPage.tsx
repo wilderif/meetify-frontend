d2a8792da3a9434e1bc05ae936e90a785deaa7c0
Yoos99 <yoosh2520@naver.com>
2024-11-06 16:41:11 +0900

    feat: 게시글 작성 페이지 서버 연동 추가

--- src/pages/PostCreatePage/CreateMeetPage.tsx
+++ src/pages/PostCreatePage/CreateMeetPage.tsx
@@ -1,21 +1,29 @@
 import { useNavigate } from "react-router-dom";
 import PostFormMeet from "../../components/layout/PostFormMeet/PostFormMeet";
 import { PostFormMeetData } from "../../types/Post";
-
+import { createMeetPost } from "../../services/postDetail/createPost";
 const CreateMeetPage: React.FC = () => {
   const navigate = useNavigate();
 
-  const handleSubmit = (data: PostFormMeetData) => {
-    console.log("Meet 데이터 제출:", data);
+  const handleSubmit = async (data: PostFormMeetData) => {
+    console.log("전송하는 데이터 확인:", data.affiliation, data.availableTime);
+    try {
+      const result = await createMeetPost(data);
+      console.log("게시글 생성 성공:", result); // 생성된 게시글 정보 확인
+      navigate(`/post/${result.id}`); // 생성된 게시글 상세 페이지로 이동
+    } catch (error) {
+      console.error("게시글 작성 오류:", error);
+      alert("게시글 작성에 실패했습니다.");
+    }
   };
 
   const handleCancel = () => {
-    navigate(-1); // 이전 페이지로 이동
+    navigate("/"); // 메인 페이지로 이동
   };
 
   return (
     <PostFormMeet
-      title="Meet"
+      title="본인"
       onSubmit={handleSubmit}
       onCancel={handleCancel}
     />
--- src/pages/PostCreatePage/CreateProjectPage.tsx
+++ src/pages/PostCreatePage/CreateProjectPage.tsx
@@ -1,16 +1,28 @@
+// src/pages/CreateProjectPage.tsx
 import { useNavigate } from "react-router-dom";
 import PostForm from "../../components/layout/PostForm/PostForm";
 import { PostFormData } from "../../types/Post";
+import { createProjectPost } from "../../services/postDetail/createPost";
+
 const CreateProjectPage: React.FC = () => {
   const navigate = useNavigate();
-  const handleSubmit = (data: PostFormData) => {
-    console.log("프로젝트 데이터 제출:", data);
+
+  const handleSubmit = async (data: PostFormData) => {
+    try {
+      const result = await createProjectPost(data);
+      console.log("게시글 생성 성공:", result); // 생성된 게시글 정보 확인
+      navigate(`/post/${result.id}`); // 생성된 게시글 상세 페이지로 이동
+    } catch (error) {
+      console.error("게시글 작성 오류:", error);
+      alert("게시글 작성에 실패했습니다.");
+    }
   };
 
   // 취소 버튼 클릭 시 이전 페이지로 이동
   const handleCancel = () => {
-    navigate(-1); // 이전 페이지로 이동
+    navigate("/"); // 메인 페이지로 이동
   };
+
   return (
     <PostForm
       title="프로젝트"
