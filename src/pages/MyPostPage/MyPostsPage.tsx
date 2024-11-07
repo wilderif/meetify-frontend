a0206a8043f09051f4a168f883aadaf1d0fce89f
ayoung <a01259@naver.com>
2024-11-07 14:41:36 +0900

    fix: 관심글, 작성글 타이틀 추가 및 데이터 없을경우 안내문구 출력

--- src/pages/MyPostPage/MyPostsPage.tsx
+++ src/pages/MyPostPage/MyPostsPage.tsx
@@ -1,5 +1,11 @@
 import { CardContainer, Pagination } from "../../components/common";
-import { StyledMyPostsContainer } from "./MyPostsPage.styles";
+import {
+  StyledMyPostsContainer,
+  Title,
+  EmptyTextWrapper,
+  EmptyBold,
+  EmptyText,
+} from "./MyPostsPage.styles";
 import useMyPostsPage from "../../hooks/useMyPostsPage";
 
 const MyPostsPage = () => {
@@ -8,13 +14,21 @@ const MyPostsPage = () => {
 
   return (
     <StyledMyPostsContainer>
-      <CardContainer postList={postList} />
-      {postList && (
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPage}
-          onPageChange={handlePageChange}
-        />
+      <Title>작성글 목록</Title>
+      {postList && postList.length > 0 ? (
+        <>
+          <CardContainer postList={postList} />
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPage}
+            onPageChange={handlePageChange}
+          />
+        </>
+      ) : (
+        <EmptyTextWrapper>
+          <EmptyBold>작성한 글이 없습니다.</EmptyBold>
+          <EmptyText>새 글을 작성하여 팀 모집을 시작해 보세요!</EmptyText>
+        </EmptyTextWrapper>
       )}
     </StyledMyPostsContainer>
   );
